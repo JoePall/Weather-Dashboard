68c50829523e2518987371d834d0e768ac307f30
Josiah Powell <josiahpowell@outlook.com>
2020-07-21 15:55:51 -0500

    Making the right API Call

--- Assets/js/index.js
+++ Assets/js/index.js
@@ -1,15 +1,21 @@
 {
     var APIKey = "3c2708df10b6a2b88d777609178106b0";
     
-    function queryOpenWeatherMapAPIByCoordinates(lon, lat, callback) {
-        var queryURL = "api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + APIKey + "&units=imperial";
+    function queryOWMAPIByCoord(lon, lat, callback) {
+        var queryURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=hourly,minutely&appid=" + APIKey + "&units=imperial";
     
-        $.getJSON(queryURL, callback);
+        $.getJSON(queryURL, response => {
+            callback(response, "local");
+        });
     }
     
-    function queryOpenWeatherMapAPIByCity(cityState, callback) {
-        var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityState + ",USA&appid=" + APIKey + "&units=imperial";
-    
-        $.getJSON(queryURL, callback);
-    } 
+    function queryOWMAPIByCity(cityState, callback) {
+        var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityState.replace(" ", "") + ",USA&appid=" + APIKey + "&units=imperial";
+
+        $.getJSON(queryURL, response => {
+            queryOWMAPIByCoord(response.city.coord.lon, response.city.coord.lat, response2 => {
+                callback(response2, cityState);
+            });
+        });
+    }
 }
